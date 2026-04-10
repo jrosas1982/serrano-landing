37899166246cf5be74228fd4ab1dcdc458b564bf
jrosas1982 <jrosas1982@gmail.com>
2026-04-10 18:09:30 -0300

    chore: reorganize project structure for scalability

---
+++ js/app.js
@@ -0,0 +1,17 @@
+﻿(function () {
+  function applyDynamicLinks() {
+    var config = window.APP_CONFIG || {};
+    var env = config.env || "prod";
+    var linksByEnv = config.links || {};
+    var links = linksByEnv[env] || linksByEnv.prod || {};
+
+    document.querySelectorAll("[data-dynamic-link]").forEach(function (el) {
+      var key = el.getAttribute("data-dynamic-link");
+      if (links[key]) {
+        el.setAttribute("href", links[key]);
+      }
+    });
+  }
+
+  window.addEventListener("DOMContentLoaded", applyDynamicLinks);
+})();
---
+++ js/config.js
@@ -0,0 +1,11 @@
+﻿window.APP_CONFIG = {
+  env: "dev",
+  links: {
+    dev: {
+      turno: "https://testingserrano.itsos.ar/"
+    },
+    prod: {
+      turno: "https://serrano.itsos.ar/"
+    }
+  }
+};
